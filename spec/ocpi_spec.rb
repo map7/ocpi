1e6a3dd04f661650f59fb14b280f4d75a0b53c3d
Michael Pope <map7777@gmail.com>
2024-06-14 13:39:33 +1000

    initial commit

---
+++ spec/ocpi_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Ocpi do
+  it "has a version number" do
+    expect(Ocpi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
